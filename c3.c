24e51056a299cb9402de45da71c1916b931a8402
debojitroyk <royk385460@gmail.com>
2025-01-05 16:11:46 +0530

    array

---
+++ c3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    float items[3];
+    printf("enter price of three items:");
+    scanf ("%f %f %f",&items[0],&items[1],&items[2]);
+
+    
+    printf("enter the total coat of %f\n",items[0]+items[0]*.18);
+    printf("enter the total coat of %f\n",items[1]+items[1]*.18);
+    printf("enter the total coat of %f\n",items[2]+items[2]*.18);
+    return 0;
+    
+
+}
